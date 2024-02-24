3745aa8df79ae47163c6a93cc5948ce475682177
Tanay0708 <123vyast@gmail.com>
2024-02-24 20:59:05 +0530

    Added

--- my-project/src/sections/Hero.jsx
+++ my-project/src/sections/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
 
   return (
-    <section className=" w-full h-screen flex max-sm:flex-col max-sm:mt-12   ">
+    <section className=" w-full h-screen flex max-sm:flex-col max-sm:mt-24   ">
       {/* text div */}
       <div className=" w-[60%] max-sm:w-full flex flex-col  justify-center  h-[300px] max-sm:mt-0 mt-20 ml-15  " >
         <h1 className="text-[3.5rem] text-coral-red max-sm:text-[28px] font-[700] max-sm:font-[400] max-lg:text-[2rem] leading-12 max-sm:leading-9">
@@ -23,10 +23,10 @@ const Hero = () => {
 
       </div>
       {/* image div */}
-      <div className=" w-[50%] h-[full] max-sm:w-full max-sm:h-[60%] relative  max-sm:ml-5  ">
-          <img src={dairy01}  className="z-10  max-lg:h-[40%] h-[80%] w-full  absolute -top-8 right-6 max-sm:-top-10 max-sm:h-[80%] max-sm:w-full   "  alt="" />
+      <div className=" w-[50%] h-[full] max-sm:w-full max-sm:h-[100%] relative  max-sm:ml-5  ">
+          <img src={dairy01}  className="z-10  max-lg:h-[40%] h-[80%] w-full  absolute -top-8 right-6 max-sm:top-10 max-sm:right-10 max-sm:w-full max-sm:h-[55%]  "  alt="" />
           <img src={line}  className= "z-0 absolute -top-60 -left-80   -rotate-[70deg] max-sm:hidden h-[800px] max-lg:hidden" alt="" />
-          <img src={plane} className="absolute w-24 -top-0 -rotate-[70deg] -left-40 max-sm:left-52 max-sm:w-12 max-sm:-top-60 max-lg:hidden max-sm:block z-0" alt="" />
+          <img src={plane} className="absolute w-24 -top-0 -rotate-[70deg] -left-40 max-sm:left-52 max-sm:w-12 max-sm:-top-52 max-lg:hidden max-sm:block z-0" alt="" />
       </div>
      
     </section>
